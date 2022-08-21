4d675ccdeff33a1ad4c0fffd4cf153a97715674d
Vyacheslav <vskorop@gmail.com>
2022-08-21 23:26:54 +0300

    fix title 2

--- src/App.js
+++ src/App.js
@@ -89,6 +89,23 @@ const App = () => {
     }
   }, [isSubMenu, mainCube])
 
+  function Stars(props) {
+    const ref = useRef()
+    const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
+    useFrame((state, delta) => {
+      ref.current.rotation.x -= delta / 10
+      ref.current.rotation.y -= delta / 15
+    })
+    return (
+      <group rotation={[0, 0, Math.PI / 4]}>
+        <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+          <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+        </Points>
+      </group>
+    )
+  }
+
+
   return (
     <>
       <a.div
@@ -98,8 +115,8 @@ const App = () => {
           {transitionText((style) => (
             <Text
             opacity={style.opacity} 
-             background={textColorSpring.background} text={textFaceMapping[mainActiveFaceIndex]?.text} />
-          ))}
+            background={textColorSpring.background} text={textFaceMapping[mainActiveFaceIndex]?.text} />
+            ))}
         </h1>
         <SubText mainActiveFaceIndex={mainActiveFaceIndex} subActiveFaceIndex={subActiveFaceIndex} isSubmenu={isSubMenu} />
       </a.div>
@@ -129,6 +146,7 @@ const App = () => {
         </Canvas>
       </div>
       <FakeContent />
+   
     </>
   )
 }
